997b4d58fc6d311acdf02940c32a89034ee45f62
Julian Lopez <jlopezsa@gmail.com>
2022-04-11 19:31:17 -0500

    feat: Create naviation bar component file

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import './NavBar.scss';
+
+function NavBar() {
+  return (
+    <div>
+      <div className="horizontalBar">
+        NavBar
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
