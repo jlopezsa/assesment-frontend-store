0c32335f8c7e109cea5a9fbfc4ca3fefc9ba03c8
Julian Lopez <jlopezsa@gmail.com>
2022-04-13 22:06:57 -0500

    feat: Use countdown timer

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,9 +2,15 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { ROUTE_PRODUCT_DETAIL } from '../../routes/routes';
 import './ProductCard.scss';
+import CountdownTimer from '../CountdownTimer/CountdownTimer';
 
 function ProductCard(props) {
   const { productsDetails } = props;
+
+  const THREE_DAYS_IN_MS = 0.001 * 24 * 60 * 60 * 1000;
+  const NOW_IN_MS = new Date().getTime();
+  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+
   return (
     <div className="contProduct">
       <div className="contProduct__image">
@@ -14,7 +20,7 @@ function ProductCard(props) {
         {productsDetails.title}
       </div>
       <div className="contProduct__time">
-        time
+        <CountdownTimer targetDate={dateTimeAfterThreeDays} />
       </div>
       <div className="contProduct__button">
         <button className="contProduct__button--push" type="button">
